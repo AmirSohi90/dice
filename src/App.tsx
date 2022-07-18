9ce9cacb1c7c0b7a29f66daac74e21b18c676c0b
Amir Sohi <amir.sohi@cazoo.co.uk>
2022-07-18 22:11:59 +0100

    removed api call from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import apis from './apis/apis';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
 
 const App = () => {
-  const res = apis.getEventsByVenue('Public Records');
-  console.log(res.then((res) => res));
-
   return (
     <QueryClientProvider client={queryClient}>
       <div>
